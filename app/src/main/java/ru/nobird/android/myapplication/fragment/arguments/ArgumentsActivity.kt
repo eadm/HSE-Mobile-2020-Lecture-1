fde7b3c46110dca05ddee25795163d5042cba24b
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-07-10 01:00:03 +0300

    Arguments finished

--- app/src/main/java/ru/nobird/android/myapplication/fragment/arguments/ArgumentsActivity.kt
+++ app/src/main/java/ru/nobird/android/myapplication/fragment/arguments/ArgumentsActivity.kt
@@ -1,7 +1,17 @@
 package ru.nobird.android.myapplication.fragment.arguments
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commitNow
+import ru.nobird.android.myapplication.R
 
 class ArgumentsActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_container)
+        supportFragmentManager.commitNow {
+            val person = Person("Clark", "Kent", 32)
+            add(R.id.container, ArgumentsFragment.newInstance(person), ArgumentsFragment.TAG)
+        }
+    }
 }
--- app/src/main/java/ru/nobird/android/myapplication/fragment/arguments/ArgumentsFragment.kt
+++ app/src/main/java/ru/nobird/android/myapplication/fragment/arguments/ArgumentsFragment.kt
@@ -5,34 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_arguments.*
 import ru.nobird.android.myapplication.R
 import ru.nobird.android.view.base.ui.extension.argument
 
 class ArgumentsFragment : Fragment() {
     companion object {
+        const val TAG = "ArgumentsFragment"
         private const val PERSON_ARG = "person"
 
-//        fun newInstance(person: Person): Fragment {
-//            val bundle = Bundle().apply {
-//                putParcelable(PERSON_ARG, person)
-//            }
-//            return ArgumentsFragment().apply { arguments = bundle }
-//        }
-
-        fun newInstance(person: Person): Fragment =
-            ArgumentsFragment().apply {
-                this.person = person
+        fun newInstance(person: Person): Fragment {
+            val bundle = Bundle().apply {
+                putParcelable(PERSON_ARG, person)
             }
+            return ArgumentsFragment().apply { arguments = bundle }
+        }
+
+//   Good way
+//        fun newInstance(person: Person): Fragment =
+//            ArgumentsFragment().apply {
+//                this.person = person
+//            }
     }
 
-//    private lateinit var person: Person
-    private var person: Person by argument()
+    private lateinit var person: Person
+
+//    Good way
+//    private var person: Person by argument()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        person = arguments?.getParcelable(PERSON_ARG) ?: throw IllegalArgumentException()
+        person = arguments?.getParcelable(PERSON_ARG) ?: throw IllegalArgumentException()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(R.layout.fragment_arguments, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        information.text = resources.getString(R.string.name_and_age, "${person.firstName} ${person.lastName}", person.age )
+    }
 }
