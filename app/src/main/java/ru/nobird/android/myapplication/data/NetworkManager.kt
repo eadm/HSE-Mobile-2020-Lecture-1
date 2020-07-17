e3b54ca1121af7ad72641baa832aa55b8838f11e
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-07-17 09:21:41 +0300

    add get

--- app/src/main/java/ru/nobird/android/myapplication/data/NetworkManager.kt
+++ app/src/main/java/ru/nobird/android/myapplication/data/NetworkManager.kt
@@ -1,16 +1,84 @@
 package ru.nobird.android.myapplication.data
 
+import androidx.annotation.WorkerThread
+import org.json.JSONArray
+import org.json.JSONObject
 import ru.nobird.android.myapplication.viewmodel.Item
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 object NetworkManager {
-    fun getItems(): List<Item> =
-        TODO()
+    private const val BASE_URL = "http://10.0.2.2:3000"
+    private const val MOVIES = "/movies"
 
-    fun createItem(item: Item) {
+    private const val FIELD_ID = "id"
+    private const val FIELD_NAME = "name"
 
+    private const val TIMEOUT = 30000
+
+    @WorkerThread
+    fun getItems(): List<Item> {
+        var connection: HttpURLConnection? = null
+
+        try {
+            connection = (URL(BASE_URL + MOVIES).openConnection() as HttpURLConnection)
+                .apply {
+                    connectTimeout = TIMEOUT
+                    requestMethod = "GET"
+                    doInput = true
+                }
+
+            if (connection.responseCode != HttpURLConnection.HTTP_OK) {
+                return emptyList()
+            }
+
+            val text = InputStreamReader(connection.inputStream).use { it.readText() }
+
+            val jsonArray = JSONArray(text)
+            val list = mutableListOf<Item>()
+
+            for (i in 0 until jsonArray.length()) {
+                val json = jsonArray.getJSONObject(i)
+                list += Item(
+                    id = json.getInt(FIELD_ID),
+                    name = json.getString(FIELD_NAME)
+                )
+            }
+            return list
+        } finally {
+            connection?.disconnect()
+        }
     }
 
-    fun deleteItem(item: Item) {
+    @WorkerThread
+    fun createItem(item: Item) {
+        var connection: HttpURLConnection? = null
+        try {
+            connection = (URL(BASE_URL + MOVIES).openConnection() as HttpURLConnection)
+                .apply {
+                    connectTimeout = TIMEOUT
+                    requestMethod = "POST"
+                    doOutput = true
+                    setRequestProperty("Content-Type", "application/json")
+                }
 
+            val data = JSONObject().apply {
+                put(FIELD_ID, item.id)
+                put(FIELD_NAME, item.name)
+            }
+
+            OutputStreamWriter(connection.outputStream).use {
+                it.write(data.toString())
+            }
+        } finally {
+            connection?.disconnect()
+        }
+    }
+
+    @WorkerThread
+    fun deleteItem(item: Item) {
+        TODO()
     }
 }
--- app/src/main/java/ru/nobird/android/myapplication/viewmodel/SampleViewModel.kt
+++ app/src/main/java/ru/nobird/android/myapplication/viewmodel/SampleViewModel.kt
@@ -3,14 +3,20 @@ package ru.nobird.android.myapplication.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.nobird.android.myapplication.data.NetworkManager
+import java.util.concurrent.Executors
 
 class SampleViewModel : ViewModel() {
+    private val executor = Executors.newSingleThreadExecutor()
+
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
 
+
     init {
         _state.value = State(emptyList())
+        fetchItems()
     }
 
     fun onCreateMovie(name: String) {
@@ -20,5 +26,16 @@ class SampleViewModel : ViewModel() {
 
     fun onClearItemsClicked() {
         _state.value = State(emptyList())
+    }
+
+    private fun fetchItems() {
+        executor.execute {
+            val items = NetworkManager.getItems()
+            _state.postValue(State(items))
+        }
+    }
+
+    override fun onCleared() {
+        executor.shutdown()
     }
 }
